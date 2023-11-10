b8cda046b4c1fdfcfa4be8de6facfec41170e586
robertbiro <biro.k.robert@gmail.com>
2023-11-10 17:11:58 +0100

    assertEquals, side effects

--- src/main/java/org/example/Dollar.java
+++ src/main/java/org/example/Dollar.java
@@ -1,2 +1,20 @@
-package org.example;public class Dollar {
+package org.example;
+
+public class Dollar {
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+    //without side effect
+        return new Dollar(amount * multiplier);
+
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == ((Dollar) object).amount;
+    }
 }
